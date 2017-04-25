f558ff5cd06918b2a66d1fae78eabfc93f80349c
Rajabu Mkomwa <rajeyw@gmail.com>
2017-04-25 12:15:50 +0300

    modified home page ui

--- src/app/home/components/package-item/package-item.component.ts
+++ src/app/home/components/package-item/package-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-package-item',
@@ -9,9 +10,15 @@ export class PackageItemComponent implements OnInit {
 
   @Input() packageItemData: any;
   selectedVersion: number;
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  viewPackage() {
+    this.router.navigate(['metadata-package/' + this.selectedVersion + '/' + this.packageItemData.id])
   }
 
 }
